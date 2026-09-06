13160e152f5cfb9038a76ae33624c1d57aaef462
danitomasetto <tomasetto.daniel@gmail.com>
2026-09-06 17:00:32 -0300

    config: connect public app to Supabase

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
-export const SUPABASE_URL = '';
-export const SUPABASE_PUBLISHABLE_KEY = '';
+export const SUPABASE_URL = 'https://nlkhbhgzscpdistzuyod.supabase.co';
+export const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_8ss7l24-Qc4EeS7l2hVzQg_lSPyn1bS';
