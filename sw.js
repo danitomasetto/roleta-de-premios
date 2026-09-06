1ed948e4a85883f156818b849679f1a927dff7c8
danitomasetto <tomasetto.daniel@gmail.com>
2026-09-06 19:29:31 -0300

    Atualiza cache do site da roleta

--- sw.js
+++ sw.js
@@ -1,1 +1,5 @@
-const CACHE='roleta-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./participar.html','./css/app.css']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='roleta-v2';
+const ASSETS=['./','./index.html','./participar.html','./css/app.css','./js/participar.js'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',event=>event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request))));
