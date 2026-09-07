33127bdbf31f5a5c080d809b950c48bb3181dd6e
danitomasetto <tomasetto.daniel@gmail.com>
2026-09-07 09:30:17 -0300

    Adiciona áudio à roleta circular

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='roleta-v2';
-const ASSETS=['./','./index.html','./participar.html','./css/app.css','./js/participar.js'];
+const CACHE='roleta-v3';
+const ASSETS=['./','./index.html','./participar.html','./css/app.css','./js/participar.js','./assets/piao-da-felicidade.mp3'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request))));
